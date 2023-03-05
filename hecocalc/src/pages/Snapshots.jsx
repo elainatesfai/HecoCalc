d260ac9906e5a19e14a15eeca6cc6ab08829febe
elainatesfai <elaina.tesfai@gmail.com>
2023-03-05 01:25:17 +0000

    listing items from s3
    The metadata isn't working yet, so I need to fix that. Also, I need to do the status tags (labels on the files) for testing. Furthermore, I need to fix that there are 5 items showing -> one being the folder itself.

--- hecocalc/src/pages/Snapshots.jsx
+++ hecocalc/src/pages/Snapshots.jsx
@@ -1,8 +1,46 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from '../components/Navbar'
 import Table from "../components/Table";
 import '../css/snapshots.css'
 import '../css/tabs.css'
+
+import { S3 } from 'aws-sdk';
+
+async function getS3Data() {
+  const s3 = new S3({
+    // accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    // secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+    // region: process.env.REACT_APP_AWS_REGION
+
+    accessKeyId: "AKIASDINEJ4Y2BGNFONK",
+    secretAccessKey: "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",
+    region: "eu-west-2"
+  });
+  const bucket = 'alancompany';
+  const prefix = 'Snapshots/';
+
+  try {
+    const s3Response = await s3.listObjectsV2({ Bucket: bucket, Prefix: prefix }).promise();
+    const data = s3Response.Contents.map(item => {
+      const nameParts = item.Key.split('/');
+      const name = nameParts[nameParts.length - 1].split('.')[0];
+      return {
+        date: item.LastModified.toLocaleDateString(),
+        name: name,
+        status: '',
+        by: null
+        // item.Metadata.uploadedby
+
+      };
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+console.log(process.env)
 
 function filterDataByStatus(status, data) {
   if (status === "All") {
@@ -12,13 +50,7 @@ function filterDataByStatus(status, data) {
   }
 }
 
-function Snapshots() {
-  const data = [
-    { date: '13/01/2023', name: 'diabetes-snapshot-v3', status: 'Rejected', by: 'john@gmail.com' },
-    { date: '11/01/2023', name: 'diabetes-snapshot-v1', status: '', by: 'john@gmail.com' },
-    { date: '12/01/2023', name: 'diabetes-snapshot-v2', status: 'Approved', by: 'john@gmail.com' }
-  ];
-
+function Snapshots(props) {
   const [searchTerm, setSearchTerm] = useState("");
   const [toggleState, setToggleState] = useState("All");
   const [tempList, setTempList] = useState([]);
@@ -26,6 +58,16 @@ function Snapshots() {
   const toggleTab = (status) => {
     setToggleState(status);
   };
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const s3Data = await getS3Data();
+      setData(s3Data);
+    }
+    fetchData();
+  }, []);
 
   const filteredData = filterDataByStatus(toggleState, data).filter((item) =>
     item.name.toLowerCase().includes(searchTerm.toLowerCase())
@@ -47,30 +89,30 @@ function Snapshots() {
           <div className="header-title">My snapshots</div>
         </div>
 
-      <div className="bloc-tabs-topic">
-        <div
-          className={toggleState === "All" ? "tabs-topic active-tabs-topic" : "tabs-topic"}
-          onClick={() => {
-            toggleTab("All");
-          }}
-        >
-          <p>All</p>
-        </div>
-        <div
-          className={toggleState === "Approved" ? "tabs-topic active-tabs-topic" : "tabs-topic"}
-          onClick={() => {
-            toggleTab("Approved");
-          }}
-        >
-          <p>Approved</p>
-        </div>
-        <div
-          className={toggleState === "Rejected" ? "tabs-topic active-tabs-topic" : "tabs-topic"}
-          onClick={() => {
-            toggleTab("Rejected");
-          }}
-        >
-          <p>Rejected</p>
+        <div className="bloc-tabs-topic">
+          <div
+            className={toggleState === "All" ? "tabs-topic active-tabs-topic" : "tabs-topic"}
+            onClick={() => {
+              toggleTab("All");
+            }}
+          >
+            <p>All</p>
+          </div>
+          <div
+            className={toggleState === "Approved" ? "tabs-topic active-tabs-topic" : "tabs-topic"}
+            onClick={() => {
+              toggleTab("Approved");
+            }}
+          >
+            <p>Approved</p>
+          </div>
+          <div
+            className={toggleState === "Rejected" ? "tabs-topic active-tabs-topic" : "tabs-topic"}
+            onClick={() => {
+              toggleTab("Rejected");
+            }}
+          >
+            <p>Rejected</p>
         </div>
       </div>
       {/* ... */}
