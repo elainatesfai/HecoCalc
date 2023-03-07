99c88c3c7e3c0f3bc3f71ffb5eb3c59eaded3845
MarwanHajMohamed <111996420+MarwanHajMohamed@users.noreply.github.com>
2023-03-07 19:10:48 +0000

    Fixed Tree CSS

--- hecocalc/src/components/TreeCostProb.jsx
+++ hecocalc/src/components/TreeCostProb.jsx
@@ -1,6 +1,6 @@
 import DataTable, { createTheme } from "react-data-table-component";
-import treecostprob from "../css/treecostprob.css"
-import React, {useState} from "react";
+import "../css/treecostprob.css";
+import React, { useState } from "react";
 import {
   fChild,
   ffChild,
@@ -14,95 +14,91 @@ import {
   ssffChild,
 } from "../data/ChildrenData";
 
-export default function TreeCostProb(){
+export default function TreeCostProb() {
+  const [pIsOpen, probIsOpen] = useState(false);
+  const [cIsOpen, costIsOpen] = useState(false);
+  const column1 = ["Dead", "Alive"];
 
+  // const deadAlive =[
+  //   //DIGIT-AET-HOSPITAL-DEAD , DIGIT-AET-NONHOSPITAL-DEAD , DIGIT-IEET-HOSPITAL-DEAD , DIGIT-IEET-NONHOSPITAL-DEAD
+  //   //DIGIT-AET-HOSPITAL-ALIVE , DIGIT-AET-NONHOSPITAL-ALIVE, DIGIT-IAET-HOSPITAL-ALIVE , DIGIT-IAET-NONHOSPITAL-ALIVE
+  //   //CURRENT-AET-HOSPITAL-DEAD ,CURRENT-AET-NONHOSPITAL-DEAD , CURRENT-IEET-HOSPITAL-DEAD , CURRENT-IEET-NONHOSPITAL-DEAD
+  //   //CURRENT-AET-HOSPITAL-ALIVE , CURRENT-AET-NONHOSPITAL-ALIVE, CURRENT-IAET-HOSPITAL-ALIVE , CURRENT-IAET-NONHOSPITAL-ALIVE
+  // ]
 
+  // const costs=[
 
-const [pIsOpen, probIsOpen]= useState(false);
-const [cIsOpen, costIsOpen]= useState(false);
-const column1 = [
-  "Dead",
-  "Alive",
-]
+  // ]
 
-// const deadAlive =[
-//   //DIGIT-AET-HOSPITAL-DEAD , DIGIT-AET-NONHOSPITAL-DEAD , DIGIT-IEET-HOSPITAL-DEAD , DIGIT-IEET-NONHOSPITAL-DEAD 
-//   //DIGIT-AET-HOSPITAL-ALIVE , DIGIT-AET-NONHOSPITAL-ALIVE, DIGIT-IAET-HOSPITAL-ALIVE , DIGIT-IAET-NONHOSPITAL-ALIVE
-//   //CURRENT-AET-HOSPITAL-DEAD ,CURRENT-AET-NONHOSPITAL-DEAD , CURRENT-IEET-HOSPITAL-DEAD , CURRENT-IEET-NONHOSPITAL-DEAD 
-//   //CURRENT-AET-HOSPITAL-ALIVE , CURRENT-AET-NONHOSPITAL-ALIVE, CURRENT-IAET-HOSPITAL-ALIVE , CURRENT-IAET-NONHOSPITAL-ALIVE
-// ]
+  const column2 = ["Digital", "Current pathway"];
 
-// const costs=[
+  const buttonlabels = ["DIGITAL", "Current pathway"];
 
-// ]
+  const probDead1 =
+    parseFloat(fChild()) * parseFloat(ffChild()) * parseFloat(fffChild());
 
-const column2 = [
-  "Digital",
-  "Current pathway"
-]
-
-const buttonlabels = [
-  "DIGITAL",
-  "Current pathway"
-]
-
-const probDead1 = parseFloat(fChild()) * parseFloat(ffChild()) * parseFloat(fffChild());
-
-
-
-    return(
-    
+  return (
     <div className="tcontainer">
       <style>
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap');
-</style>
-      
-      <table className="table-box0">
-        
-        {buttonlabels.map((label, index)=>(
-        <tr key={index}>
-          <td>
-            <button className="btn-labels" style={{width: (index ===0) ? '80px' : '120px'}}>{label}</button></td>
-        </tr>
-        ))}
-      </table>  
+        @import
+        url('https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap');
+      </style>
 
-      <table className="table-box1"> 
+      <table className="table-box0">
+        {buttonlabels.map((label, index) => (
+          <tr key={index}>
+            <td>
+              <button
+                className="btn-labels"
+                style={{ width: index === 0 ? "80px" : "120px" }}
+              >
+                {label}
+              </button>
+            </td>
+          </tr>
+        ))}
+      </table>
+
+      <table className="table-box1">
         <thead>
           <tr>
-           <th></th>
-           <th></th>
-           <th><button className="btn-probabilities">Probabilities</button></th>  
+            <th></th>
+            <th></th>
+            <th>
+              <button className="btn-probabilities">Probabilities</button>
+            </th>
           </tr>
-          
         </thead>
         <tbody>
-        
           {/* column labels Dead Alive*/}
-          {column1.map((label)=> (
-          <tr>
-            <td></td>
-            <td className="t-input">{label}</td>
+          {column1.map((label) => (
+            <tr>
+              <td></td>
+              <td className="t-input">{label}</td>
 
-            {/* Trying to map an array that will contain formulas to calculate designated cells */}
-            {/* {body.map((values,valueID) =><TableRow rowContent={values} key={valueID}/> )} */}
-            {/* The above goes inside the following class. This "body" will have formulas for each in there. These have been mentioned above under body */}
-            <td className="t-input">{probDead1}</td>   
-          </tr>
+              {/* Trying to map an array that will contain formulas to calculate designated cells */}
+              {/* {body.map((values,valueID) =><TableRow rowContent={values} key={valueID}/> )} */}
+              {/* The above goes inside the following class. This "body" will have formulas for each in there. These have been mentioned above under body */}
+              <td className="t-input">{probDead1}</td>
+            </tr>
           ))}
-          <tr style={{ height: '10px' }}></tr> 
-          {column1.map((label, index)=> (
-          <tr key={index}>
-            <td></td>
-            <td className="t-input">{label}</td>
-            <td className="t-input"></td>
-          </tr>
-          ))} 
+          <tr style={{ height: "10px" }}></tr>
+          {column1.map((label, index) => (
+            <tr key={index}>
+              <td></td>
+              <td className="t-input">{label}</td>
+              <td className="t-input"></td>
+            </tr>
+          ))}
           <tr>
             <td></td>
             <td></td>
-            <td><button className="Link" onClick={()=> probIsOpen(true)}>View More</button></td>
-            </tr>           
+            <td>
+              <button className="Link" onClick={() => probIsOpen(true)}>
+                View More
+              </button>
+            </td>
+          </tr>
         </tbody>
       </table>
 
@@ -110,57 +106,61 @@ const probDead1 = parseFloat(fChild()) * parseFloat(ffChild()) * parseFloat(fffC
         <thead>
           <tr>
             <th></th>
-            <th><button className="btn-interventions">Interventions</button></th>
-            <th><button className="btn-cost">Total Cost</button></th>
-            <th><button className="btn-qalys">Total QALYs</button></th>
+            <th>
+              <button className="btn-interventions">Interventions</button>
+            </th>
+            <th>
+              <button className="btn-cost">Total Cost</button>
+            </th>
+            <th>
+              <button className="btn-qalys">Total QALYs</button>
+            </th>
           </tr>
         </thead>
         <tbody>
-        
           {/* column labels Dead Alive*/}
-          {column2.map((label)=> (
+          {column2.map((label) => (
+            <tr>
+              <td></td>
+
+              <td className="t-input">{label}</td>
+              <td className="t-input"></td>
+              <td className="t-input"></td>
+            </tr>
+          ))}
           <tr>
             <td></td>
-           
-            <td className="t-input">{label}</td>
-            <td className="t-input"></td>
-            <td className="t-input"></td>
-        
+            <td></td>
+            <td>
+              <button className="Link" onClick={() => costIsOpen(true)}>
+                View More
+              </button>
+            </td>
           </tr>
-          
-          ))}
-           <tr>
-            <td></td>
-            <td></td>
-            <td><button className="Link" onClick={()=> costIsOpen(true)}>View More</button></td>
-            </tr>      
         </tbody>
       </table>
 
-    
-
-
-      <table className ="table-box3">
+      <table className="table-box3">
         <thead>
           <tr>
-          <th></th>
-          <th><button className="btn-cost-qalys">Cost/QALYs</button></th>
+            <th></th>
+            <th>
+              <button className="btn-cost-qalys">Cost/QALYs</button>
+            </th>
           </tr>
         </thead>
-        
+
         <tbody>
           <tr>
-          <td></td>
-          <td className="t-input"></td>
-          <td></td>
+            <td></td>
+            <td className="t-input"></td>
+            <td></td>
           </tr>
         </tbody>
-        
       </table>
-      
 
       {/* FOR POPUP INCASE WE NEED IT */}
-     {/* {cIsOpen && (
+      {/* {cIsOpen && (
          <div className="tableCost">
           <div>
             This is the content of the pop-up.
@@ -181,6 +181,6 @@ const probDead1 = parseFloat(fChild()) * parseFloat(ffChild()) * parseFloat(fffC
           </button>
          </div>
         )} */}
-    </div> 
-    )
+    </div>
+  );
 }
