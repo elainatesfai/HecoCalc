5dc8d5ddc45839c02e22b3ed1bfed609f349b161
LaurenRuuyx <98824446+LaurenRuuyx@users.noreply.github.com>
2023-03-13 16:34:09 +0000

    Update Repositories.jsx

--- hecocalc/src/components/Repositories.jsx
+++ hecocalc/src/components/Repositories.jsx
@@ -6,6 +6,10 @@ export default function Repositories(choices) {
   const selectValue = (event) => {
     if(event.target.Value !== 'Select'){
       localStorage.setItem("repoName", event.target.value);
+      var comp = localStorage.getItem("company");
+      var rep = localStorage.getItem("repoName");
+      localStorage.setItem("s3Link",(comp + "/Repositories/" + rep));
+      console.log(localStorage.getItem("s3Link"));
 
     }
   };
